f3c56256b8938db3e213a564215382e8070452cb
Alex Burrows <burrows.alexj@gmail.com>
2024-11-29 22:37:05 -0500

    create and style entry component

--- travel-journal/src/components/Header.jsx
+++ travel-journal/src/components/Header.jsx
@@ -1,10 +1,10 @@
 import globe_img from "../assets/globe.png"
 
-export default function Header(){
-    return(
+export default function Header() {
+    return (
         <header>
-            <img src={globe_img} alt="Globe" className="globe-icon"/>
-            <h2 className="header-title">my travel journal.</h2>
+            <img src={globe_img} alt="globe icon" />
+            <h1>my travel journal.</h1>
         </header>
     )
 }
