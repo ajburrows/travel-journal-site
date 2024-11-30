740fc1ccd419b4920d9cf08ddd6c596e9aae17ce
Alex Burrows <burrows.alexj@gmail.com>
2024-11-30 11:39:44 -0500

    pass entry props with spread

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -7,7 +7,7 @@ export default function App(){
     const travel_entries = data.map(entry_data => {
         return <Entry 
                 key={entry_data.id}
-                entry={entry_data}
+                {...entry_data}
                 />
     })
     return(
